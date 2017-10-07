8f1f343bffa684c02212b3f41b5d5bcd65f3233f
Matt Sell <mwsell@gmail.com>
2017-10-07 18:14:59 -0500

    add table formatting

--- main.mjs
+++ main.mjs
@@ -1,4 +1,5 @@
 import fetch from 'isomorphic-fetch';
+import Table from 'cli-table2'
 
 async function getData() {
     const res = await fetch('https://nfl-api.now.sh/api/nfl-odds')
@@ -8,12 +9,17 @@ async function getData() {
 
 getData()
     .then(data => {
-       const games = data.filter(x => (x.total > 0)).sort(x => (x.home_spread))
-       
+       const games = data.filter(x => (x.total > 0)).sort(x => (x.home_spread))      
+       const table = new Table({
+         head: ['Away', 'Home', 'Home Spread']
+       })
+
         for(const game of games){
             const { away_short_name, home_short_name, home_spread} = game
-            console.log(`${away_short_name} @ ${home_short_name} ${home_spread}`)
+            table.push([away_short_name, home_short_name, home_spread])            
         }
+
+        console.log(table.toString())
     })
     .catch(e => console.error(e))      
   
