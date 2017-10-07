aa471b6076db96f59b91240ee1dcfc3ecb9e1241
Matt Sell <mwsell@gmail.com>
2017-10-07 17:55:35 -0500

    change to my api

--- index.js
+++ index.js
@@ -3,20 +3,14 @@ require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
 async function getData() {
-    const settings = {
-        headers: { Referer: 'http://www.oddsshark.com/nfl/odds' }
-    }
-
-    const res = await fetch('http://io.oddsshark.com/upcoming/us/nfl', settings)
+    const res = await fetch('https://nfl-api.now.sh/api/nfl-odds')
     const data = await res.json()
     return data
   }
-  
-
 
 getData()
     .then(data => {
-        games = data.data.filter(x => (x.total > 0)).sort(x => (x.home_spread))
+        games = data.filter(x => (x.total > 0)).sort(x => (x.home_spread))
        
         for(const game of games){
             const { away_short_name, home_short_name, home_spread} = game
